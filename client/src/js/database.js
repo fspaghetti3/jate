617ca55b0bfb5cac831dcb21388fa2c2ac1147b1
Fred <fsolleder3@gmail.com>
2023-11-18 10:49:01 -0800

    implemented database.js functions.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+    const editorDb = await openDB('JATE', 1);
+    const transfer = editorDb.transaction('JATE', 'readwrite')
+    const store = transfer.objectStore('JATE')
+    const req = store.put({ id: 1, value: content})
+    const result = await req;
+
+    console.error('Saved to Database', result.value );
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+    const editorDb = await openDB('JATE', 1);
+    const transfer = editorDb.transaction('JATE', 'readwrite')
+    const store = transfer.objectStore('JATE')
+    const req = store.put({ id: 1, value: content})
+    const result = await req;
+    result ? console.log('Data retrieved from Database!', result.value) : console.log('Data not found.', result.value)
+
+}
 
 initdb();
